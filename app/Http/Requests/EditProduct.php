edc8785c61255d5f45b52ed0c39673976a45d0e9
ThanhBinhKma <thanhbinhkma27@gmail.com>
2019-05-12 12:53:08 +0700

    first commit

---
+++ app/Http/Requests/EditProduct.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditProduct extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nameProdcuts' =>'required',
+            'cate' =>'required',
+            'brands' =>'required',
+            'color' =>'required',
+            'size' =>'required',
+            'price' =>'required',
+            'qty' =>'required',
+            'saleOff' =>'required',
+            'images' =>'required',
+            'status' =>'required',
+            'new' =>'required',
+            'highlight' =>'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nameProdcuts.required' =>':attribute không được để trống',
+            'cate.required' =>':attribute không được để trống',
+            'brands.required' =>':attribute không được để trống',
+            'color.required' =>':attribute không được để trống',
+            'size.required' =>':attribute không được để trống',
+            'price.required' =>':attribute không được để trống',
+            'qty.required' =>':attribute không được để trống',
+            'saleOff.required' =>':attribute không được để trống',
+            'images.required' =>':attribute không được để trống',
+            'status.required' =>':attribute không được để trống',
+            'new.required' =>':attribute không được để trống',
+            'highlight.required' =>':attribute không được để trống'
+        ];
+    }
+}
---
+++ public/js/click-menu.js
@@ -0,0 +1,91 @@
+// document.addEventListener("DOMContentLoaded",function(){
+// 	var clickLi = document.querySelectorAll('.hover-cusor');
+// 	console.log(clickLi);
+// 	var showUl = document.querySelectorAll('.hiddenUl');
+// 	console.log(showUl);
+// 	clickLi.onclick=function(){
+
+// 		showUl[0].classList.remove('showUl');
+// 	}
+// });
+// $('.hover-cusor').click(function(){
+// 	alert('hihi');
+// });
+$('.hover-cusor').click(function(){
+      $('ul.hiddenUl ').toggleClass("showUl");
+});
+$(".menu-drop-left div").click(function() {
+            $(".ul-hihi-left").slideToggle();
+            $(".sub-ul-drop-left").css("display", "none");
+      });
+
+      $(".ul-hihi-left li").click(function() {
+            $(".sub-ul-drop-left").slideUp();
+            $(this).find('.sub-ul-drop-left').slideToggle();
+      });
+
+      $(window).resize(function() {
+            if($(window).width() > 768) {
+                  $(".ul-hihi-left").removeAttr('style');
+            }
+ });
+
+$(".menu-drop-right div").click(function() {
+    $(".ul-hihi-right").slideToggle();
+});
+
+
+$(window).resize(function() {
+    if($(window).width() > 768) {
+          $(".ul-hihi").removeAttr('style');
+    }
+});
+// $(window).reload(function(){
+
+//   if($(window).width() > 990){
+//     $('.jq-one').click(function(){
+//       $(this).addClass('click-color-menu');
+//       $('.jq-two').remove('click-color-menu');
+//       $('.jq-three').remove('click-color-menu');
+//       $('.jq-four').remove('click-color-menu');
+//     });
+//     $('.jq-two').click(function(){
+//       $(this).addClass('click-color-menu');
+//       $('.jq-one').remove('click-color-menu');
+//       $('.jq-three').remove('click-color-menu');
+//       $('.jq-four').remove('click-color-menu');
+//     })
+//     ;$('.jq-three').click(function(){
+//       $(this).addClass('click-color-menu');
+//       $('.jq-one').remove('click-color-menu');
+//       $('.jq-two').remove('click-color-menu');
+//       $('.jq-four').remove('click-color-menu');
+//     })
+//     ;$('.jq-four').click(function(){
+//       $(this).addClass('click-color-menu');
+//       $('.jq-one').remove('click-color-menu');
+//       $('.jq-two').remove('click-color-menu');
+//       $('.jq-three').remove('click-color-menu');
+//     });
+//   }
+// });
+if($(window).width()>990){
+$(document).ready(function(){
+  var a = window.location.href;
+  var b = a.slice(22);
+
+  if(b === 'san-pham'){
+    $('.jq-one').removeClass('click-color-menu');
+    $('.jq-two').addClass('click-color-menu');
+  }
+  if(b==='gioi-thieu'){
+    $('.jq-one').removeClass('click-color-menu');
+    $('.jq-three').addClass('click-color-menu');
+  }
+  if(b==='lien-he'){
+    $('.jq-one').removeClass('click-color-menu');
+    $('.jq-four').addClass('click-color-menu');
+  }
+  
+});
+}
